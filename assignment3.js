1ab8b28946f71945c46037ee4da43e0ba0e74813
web coders <webcoders1122@gmail.com>
2024-06-08 11:57:08 +0500

    assignment 3 completed

---
+++ assignment3.js
@@ -0,0 +1,5 @@
+const os = require("node:os");
+// console.log(os.arch()); shows cpu type
+// console.log(os.cpus()); show complete cpu info
+// console.log(os.networkInterfaces()); provides network info
+console.log(os.totalmem());
