48e0f2de07294f6ce1c31245faec5b517a5ea32b
web coders <webcoders1122@gmail.com>
2024-06-10 12:00:59 +0500

    url switch is working for static, api and home page

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 const http = require("http");
+const fs = require("fs");
+
+const products = fs.readFileSync("data.json", "utf-8");
+const errorPage = fs.readFileSync("errorPage.html", "utf-8");
 
 const server = http.createServer((req, res) => {
+  //   console.log(req.method);
+  switch (req.url) {
+    case "/":
+      res.end("Hello server");
+      break;
+    case "/api":
+      res.setHeader("Content-Type", "application/json");
+      res.end(products);
+      break;
+
+    default:
+      res.setHeader("Content-Type", "text/html");
+      res.writeHead(404);
+      res.end(errorPage);
+      break;
+  }
   //   console.log(req, res);
-  res.setHeader("Content-Type", "text/html");
-  res.end("hello");
+  //   res.setHeader("Content-Type", "text/html");
 });
 server.listen(3030);
