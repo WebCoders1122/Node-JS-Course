29d57f5fcc644a73bee0605394a559ee1a48f16c
web coders <webcoders1122@gmail.com>
2024-06-10 11:30:46 +0500

    basic server setup complete

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("Node JS Course");
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  //   console.log(req, res);
+  res.setHeader("Content-Type", "text/html");
+  res.end("hello");
+});
+server.listen(3030);
