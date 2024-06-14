a2616980c47a1ce8ddf72d7c406e8043afd0966c
web coders <webcoders1122@gmail.com>
2024-06-14 14:39:52 +0500

    router applied via seperate folder

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const quoteController = require("./controller/quotes");
+const quotesRoutes = require("./routes/quotes");
 
 const app = express();
 
 // middlewares
 app.use(bodyParser.json());
-
-// CRUD = create read update and delete operations || REST APIs
-app.post("/quotes", quoteController.create);
-app.get("/quotes", quoteController.getAll);
-app.get("/quotes/:id", quoteController.getOne);
-app.put("/quotes/:id", quoteController.replace);
-app.patch("/quotes/:id", quoteController.update);
-app.delete("/quotes/:id", quoteController.remove);
+app.use("/quotes", quotesRoutes.router);
 
 app.listen(8080, (error) => {
   if (error) {
---
+++ routes/quotes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const quoteController = require("../controller/quotes");
+const router = express.Router();
+
+// CRUD = create read update and delete operations || REST APIs
+router
+  .post("", quoteController.create)
+  .get("", quoteController.getAll)
+  .get("/:id", quoteController.getOne)
+  .put("/:id", quoteController.replace)
+  .patch("/:id", quoteController.update)
+  .delete("/:id", quoteController.remove);
+
+exports.router = router;
