2c15caba3709c383986a461e9b69420ba5f157a1
web coders <webcoders1122@gmail.com>
2024-06-14 14:30:20 +0500

    controller is applied

---
+++ controller/quotes.js
@@ -0,0 +1,38 @@
+const fs = require("fs");
+const { quotes } = JSON.parse(fs.readFileSync("data.json", "utf-8"));
+
+//functions
+exports.create = (req, res) => {
+  console.log(req.body);
+  quotes.push(req.body);
+  res.status(201).json(req.body);
+};
+exports.getAll = (req, res) => {
+  res.status(200).json(quotes);
+};
+exports.getOne = (req, res) => {
+  const id = req.params.id;
+  let index = quotes.findIndex((quote) => quote.id == id);
+  res.status(200).json(quotes[index]);
+};
+exports.replace = (req, res) => {
+  const updatedQuote = req.body;
+  const id = req.params.id;
+  const index = quotes.findIndex((q) => q.id == id);
+  quotes.splice(index, 1, updatedQuote);
+  res.status(201, "Replaced").json(updatedQuote);
+};
+exports.update = (req, res) => {
+  const quoteUpdate = req.body;
+  const id = req.params.id;
+  const index = quotes.findIndex((q) => q.id == id);
+  const updatedQuote = { ...quotes[index], ...quoteUpdate };
+  quotes.splice(index, 1, updatedQuote);
+  res.status(201, "updated").json(updatedQuote);
+};
+exports.remove = (req, res) => {
+  const index = quotes.findIndex((q) => q.id == req.params.id);
+  const deletedQuote = quotes[index];
+  quotes.splice(index, 1);
+  res.status(202, "Deleted").json(deletedQuote);
+};
--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("Node JS Course");
+const express = require("express");
+const bodyParser = require("body-parser");
+const quoteController = require("./controller/quotes");
+
+const app = express();
+
+// middlewares
+app.use(bodyParser.json());
+
+// CRUD = create read update and delete operations || REST APIs
+app.post("/quotes", quoteController.create);
+app.get("/quotes", quoteController.getAll);
+app.get("/quotes/:id", quoteController.getOne);
+app.put("/quotes/:id", quoteController.replace);
+app.patch("/quotes/:id", quoteController.update);
+app.delete("/quotes/:id", quoteController.remove);
+
+app.listen(8080, (error) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("server Started");
+  }
+});
