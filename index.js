78a6c1fa212d9d63846f1e9279029261734e702a
web coders <webcoders1122@gmail.com>
2024-07-04 14:08:58 +0500

    sessions with express-session

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const path = require("path");
 const { auth } = require("./controller/auth"); //auth middleware from auth controller
+const session = require("express-session");
 
 const app = express();
 
@@ -17,14 +18,34 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false, maxAge: 60000 },
+  })
+);
 app.use(express.static(path.resolve(__dirname, "dist")));
 app.use("/quotes", auth, quotesRoutes.router);
 app.use("/products", auth, productsRoutes.router);
 app.use("/tasks", auth, taskRouter.router);
 app.use("/users", auth, userRouter.router);
 app.use("/auth", authRouter.router);
-app.use("*", (req, resolve) => {
-  resolve.sendFile(path.resolve(__dirname, "dist", "index.html"));
+app.get("/session", function (req, res, next) {
+  if (req.session.views) {
+    req.session.views++;
+    res.setHeader("Content-Type", "text/html");
+    res.write("<p>views: " + req.session.views + "</p>");
+    res.write("<p>expires in: " + req.session.cookie.maxAge / 1000 + "s</p>");
+    res.end();
+  } else {
+    req.session.views = 1;
+    res.end("welcome to the session demo. refresh!");
+  }
+});
+app.use("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "dist", "index.html"));
 });
 
 app.listen(process.env.PORT, (error) => {
